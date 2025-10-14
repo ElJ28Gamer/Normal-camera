b76d0f08bcfe99b2522f1487b08799fdfde29ebc
ElJ28Gamer <222879086+ElJ28Gamer@users.noreply.github.com>
2025-10-14 20:10:26 +0200

    Made from the official documentation

---
+++ Camera.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("NO")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("exit")
+        break
+
+    color = cv.cvtColor(frame, cv.COLOR_BGRA2RGBA)
+    cv.imshow("CUSTOM CAMERA", color)
+    if cv.waitKey(1) == ord("q") or cv.waitKey(1) == ord("Q"):
+        break
+
+cap.release()
+cv.destroyAllWindows()
