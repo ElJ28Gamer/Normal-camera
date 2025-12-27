16789bba0e40e861f8a04d0c223ce6565387c2cf
ElJ28Gamer <elj28fer@gmail.com>
2025-12-27 21:49:10 +0100

    test

--- Camera.py
+++ Camera.py
@@ -8,12 +8,19 @@ if not cap.isOpened():
 
 while True:
     ret, frame = cap.read()
-
+    
+    if cv.waitKey(1) == ord("n") or cv.waitKey(1) == ord("N"):
+        custom_color = cv.COLOR_BGR2BGRA
+    
+    if cv.waitKey(1) == ord("o") or cv.waitKey(1) == ord("O"):
+        custom_color = cv.COLOR_
+    
+    
     if not ret:
         print("exit")
         break
 
-    color = cv.cvtColor(frame, cv.COLOR_BGR2BGRA)
+    color = cv.cvtColor(frame, custom_color)
     cv.imshow("CUSTOM CAMERA", color)
     if cv.waitKey(1) == ord("q") or cv.waitKey(1) == ord("Q"):
         break
